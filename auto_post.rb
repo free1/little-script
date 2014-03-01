ef2baa8e2d54c5bd1ca552864364b49cbd3300c9
chen xu <freeloverails@gmail.com>
2014-03-01 09:46:44 +0800

    Update auto_post.rb
    去掉权限

--- auto_post.rb
+++ auto_post.rb
@@ -11,3 +11,5 @@ params =
       "card_reply[state]" => 1
   }
 x = Net::HTTP.post_form(URI.parse('http://localhost:3000/cards/82/card_replies.json'), params)
+
+# 当模拟的请求需要验证时，可以在控制器中加入skip_before_filter :verify_authenticity_token, :only => [:create]
